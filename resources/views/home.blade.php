4f740109b51ef31ea88607b9734404fcae6a48aa
Bilobra <bra.carrozzo@gmail.com>
2022-10-11 16:27:02 +0200

    fix style + cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,27 @@
 @section('metaTitle','Home page')
 
 @section('content')
-    <h2>I nostri film:</h2>
-    @forelse ($movies as $movie)
+<section class="black_section">
+        <h2 class="title_section">I nostri film:</h2>
         <div class="container">
-            <h3>Title : {{$movie->title}}</h3>
-            <h4>Original Title : {{$movie->original_title}}</h4>
-            <p>Nationality : {{$movie->nationality}}</p>
-            <p>Date : {{$movie->date}}</p>
-            <p>Vote : {{$movie->vote}}</p>
-            <br>
+            <div class="main_content">
+                @forelse ($movies as $movie)
+                    <div class="card">
+                        <a href="">
+                            <h3>Title : {{$movie->title}}</h3>
+                            <h4>Original Title : {{$movie->original_title}}</h4>
+                            <p>Nationality : {{$movie->nationality}}</p>
+                            <p>Date : {{$movie->date}}</p>
+                            <p>Vote : {{$movie->vote}}</p>
+                        </a>
+                        
+                    </div>
+                @empty
+                    <p>non ci sono elementi</p>
+                @endforelse
+
+            </div>
         </div>
-    @empty
-        
-    @endforelse
+    </section>
 @endsection
     
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,11 +2,11 @@
 
 <header>
     <div class="container">
-        <div class="nav_wrapper">
+        <div class="nav-wrapper">
             <nav class="nav">
                <ul>
                    <li class="nav-link">
-                           <a href="{{route('home')}}">Home</a>
+                        <a href="{{route('home')}}">Home</a>
                    </li>
                </ul>
            </nav>
